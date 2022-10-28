20713e7305936d7b79aa6048a23ad64dbf79dfe2
Reid Mewborne <9111792+8bitreid@users.noreply.github.com>
2022-10-28 11:09:46 -0400

    cleanup, +

---
+++ src/main/scala/examples/manydeps/NeedyApp.scala
@@ -0,0 +1,11 @@
+package examples.manydeps
+
+import zio.{ZIO, ZIOAppDefault}
+
+object NeedyApp extends ZIOAppDefault {
+  // TODO: multi level dependency app
+  override def run: ZIO[Any, Any, Any] = ???
+
+
+
+}
